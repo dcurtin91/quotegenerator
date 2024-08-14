14c3c9fd1729cf1012d6f50527d3892d4f87b480
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-14 11:39:51 -0500

    finished

--- quotegenerator/src/App.tsx
+++ quotegenerator/src/App.tsx
@@ -1,72 +1,44 @@
 import { useState } from 'react'
 import './App.css'
+import quotes from './assets/quotes.json'
+  
 
-
-const quotes = [
-  {
-    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-    author: 'Nelson Mandela'
-  },
-  {
-    quote: 'The way to get started is to quit talking and begin doing.',
-    author: 'Walt Disney'
-  },
-  {
-    quote: 'The future belongs to those who believe in the beauty of their dreams.',
-    author: 'Eleanor Roosevelt'
-  },
-  {
-    quote: 'You must be the change you wish to see in the world.',
-    author: 'Mahatma Gandhi'
-  },
-  {
-    quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier.',
-    author: 'Mother Theresa'
-  },
-  {
-    quote: 'The only thing we have to fear is fear itself.',
-    author: 'Franklin D. Roosevelt'
-  },
-  {
-    quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
-    author: 'Martin Luther King Jr.'
-  }
-];
-
-
-
-function getRandomIndex() {
-  return (
-    Math.floor(Math.random() * quotes.length)
-  );
-};
-
-function handleLoad() {
-  const randomIndex = getRandomIndex();
-  return (
-    {
-      quote: quotes[randomIndex].quote,
-      author: quotes[randomIndex].author
-    }
-  )
+interface Quote {
+  quote: string;
+  author: string;
 }
 
+
+
+
+const getRandomIndex = (): Quote => {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
+
+
+
 function App() {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
+  const [quote, setQuote] = useState<Quote>(getRandomIndex());
+ 
+  const changeQuote = () => {
+    setQuote(getRandomIndex());
+  }
 
   return (
     <>
-      <div id="quote-box" onLoad={this.handleLoad} style={styles.quoteBox}>
-        <div id="text" style={styles.text}>{`"${this.state.quote}"`}</div>
-        <div id="author" style={styles.author}>- {this.state.author}</div>
-        <button id="new-quote" onClick={this.handleLoad} style={styles.button}>New Quote</button>
-        <a href={`https://x.com/intent/post?text=${this.state.quote}${` `}-${this.state.author}`} id="tweet-quote" target="_blank" style={styles.tweetLink}>
+      <div id="quote-box">
+        <div id="text">{quote.quote}</div>
+        <div id="author">- {quote.author}</div>
+        <button id="new-quote" onClick={changeQuote}>New Quote</button>
+        <a href={`https://x.com/intent/post?text=${quote.quote}${` `}-${quote.author}`} id="tweet-quote" target="_blank" className='tweet-link'>
           Tweet this Quote
         </a>
       </div>
     </>
   )
+  
 }
+
+
 
 export default App
